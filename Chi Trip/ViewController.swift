a83f3f1911e71665c03b814dfd9e707a0b363307
Gabbie <air@airs-macbook-air.local>
2018-07-23 12:23:38 -0700

    Initial Commit

---
+++ Chi Trip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chi Trip
+//
+//  Created by Gabbie on 7/23/18.
+//  Copyright © 2018 Gabbie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
